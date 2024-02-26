b50f74e0fae98a75b62d1c3dc62c8a90cc479d0f
Rohan Kulkarni <92090235+Ro1kulkarni@users.noreply.github.com>
2024-02-26 11:18:55 +0530

    Update Feature_Extraction.py

--- Feature_Extraction.py
+++ Feature_Extraction.py
@@ -1,21 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[32]:
-
-
-import tensorflow    #It is used for building & traning deep learming models. 
-from tensorflow.keras.preprocessing import image    #It is used for loading and preprocessing images.
-from tensorflow.keras.layers import GlobalMaxPooling2D    #It is used for spatial data reduction in convolution neural networks.
+import tensorflow                                                            #It is used for building & traning deep learming models. 
+from tensorflow.keras.preprocessing import image                             #It is used for loading and preprocessing images.
+from tensorflow.keras.layers import GlobalMaxPooling2D                       #It is used for spatial data reduction in convolution neural networks.
 from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input #It is used for preprocess_input function for image preprocessing.
-import numpy as np          #It is used for numerical computaions and array manipulation.
-from numpy.linalg import norm        #The 'norm' function from Numpy's linear algebra modul for vector normalization.
-import os          #It is used for interacting with the opreting system, such as file handling directory operations. 
-from tqdm import tqdm        #It is used for displaying progress bars during iterations.
-import pickle           #It is used for serializing ans deserializing Python objects.
-
-
-# In[33]:
+import numpy as np                                                           #It is used for numerical computaions and array manipulation.
+from numpy.linalg import norm                                                #The 'norm' function from Numpy's linear algebra modul for vector normalization.
+import os                                                         #It is used for interacting with the opreting system, such as file handling directory operations. 
+from tqdm import tqdm                                                        #It is used for displaying progress bars during iterations.
+import pickle                                                                #It is used for serializing ans deserializing Python objects.
 
 
 # Load the ResNet50 model pre-trained on the ImageNet dataset,
@@ -34,10 +25,6 @@ model = tensorflow.keras.Sequential([
 # Print a summary of the model architecture, including information about each layer
 print(model.summary())
 
-
-# In[34]:
-
-
 def extract_features(img_path, model):
     img = image.load_img(img_path, target_size=(224, 224))  # Resizing the image to (224, 224)
     img_array = image.img_to_array(img)                     # Convert the image to a numpy array
@@ -46,9 +33,6 @@ def extract_features(img_path, model):
     result = model.predict(preprocessed_img).flatten()      # Use the pre-trained model to extract features from the preprocessed image 
     normalized_result = result / np.linalg.norm(result)     # Normalize the extracted features to ensure they have unit norm
     return normalized_result                                # Return the normalized feature vector
-
-
-# In[35]:
 
 
 filenames = []
@@ -60,10 +44,6 @@ for file in os.listdir(images):
 print("Number of Images =",len(filenames))
 print("Path of first five images =",filenames[0:5])
 
-
-# In[38]:
-
-
 feature_list = []
 for file in tqdm(filenames):                         # Iterate through each file in the list of filenames
     feature_list.append(extract_features(file,model))# Extract features from the image file using the pre-trained model and append them to the feature list
@@ -72,16 +52,8 @@ for file in tqdm(filenames):                         # Iterate through each file
 pickle.dump(feature_list,open("embeddings.pkl",'wd'))# Save the feature list to a file using pickle
 pickle.dump(filenames,open("filnames.pkl",'wb'))     # Save the list of filenames to a file using pickle
 
-
-# In[42]:
-
-
 #pickle.dump(feature_list, open("embeddings.pkl", 'wb'))
 #pickle.dump(filenames, open("filenames.pkl", 'wb'))
-
-
-# In[41]:
-
 
 # Specify the directory Path
 save_dir = "D:/"
@@ -95,9 +67,4 @@ pickle.dump(feature_list, open(os.path.join(save_dir, "embeddings.pkl"), 'wb'))
 # Save filenames to filenames.pkl in the specified directory
 pickle.dump(filenames, open(os.path.join(save_dir, "filenames.pkl"), 'wb'))
 
-
-# In[51]:
-
-
 print("Shape for feature_list =",np.array(feature_list).shape)
-
